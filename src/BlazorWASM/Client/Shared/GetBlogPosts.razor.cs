e8c41ce619f5652dc934ee0c4ba8f59fddc0994c
#Thor Emil Lægteskov Christiansen <telc26793@edu.ucl.dk>
2022-08-26 10:58:48 +0200

    Removed the idea of using api.

---
+++ src/BlazorWASM/Client/Shared/GetBlogPosts.razor.cs
@@ -0,0 +1,38 @@
+﻿using BlazorWASM.Shared.Models;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorWASM.Client.Shared;
+
+public partial class GetBlogPosts
+{
+    [Parameter]
+    public int PostsAmount { get; set; }
+
+    [Parameter]
+    public int PostsOffset { get; set; }
+
+    private List<PostModel> _blogPosts;
+
+    public GetBlogPosts()
+    {
+        _blogPosts = new();
+    }
+
+    protected override async Task OnInitializedAsync()
+    {
+        // Contact database.
+        var dsa = "dsa";
+
+        // Update list.
+
+        StateHasChanged();
+    }
+
+    // When this component is loaded we need to do some things.
+    // If we have a PostsAmount, then contact the database and retrieve that many posts.
+    // If we have an offset as well, then add the offset to the query.
+
+    // After that, throw the resulting data into a list.
+    // Use that list to populate the html.
+    // Make sure view is notified about the change.
+}
---
+++ src/BlazorWASM/Shared/Models/PostModel.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorWASM.Shared.Models;
+
+public class PostModel
+{
+    public int Id { get; set; }
+
+    [Required]
+    public string Title { get; set; }
+
+    [Required]
+    public string Subtitle { get; set; }
+
+    [Required]
+    public string Preview { get; set; }
+
+    [Required]
+    public string ImageName { get; set; }
+
+    public DateTime DateCreated { get; set; } = DateTime.Now;
+
+    [Required]
+    public string Post { get; set; }
+}
